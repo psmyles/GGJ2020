de2ea727305d2092745ed1be7bb3902b77f1e654
DESKTOP-99KJ4FJ\parag <paragb>
2020-02-01 21:05:37 +0530

    Changes to focus camera on the player. To change the view size change FOV property.

--- GGJ2020/Assets/Scripts/Gameplay/CameraControl.cs
+++ GGJ2020/Assets/Scripts/Gameplay/CameraControl.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System;
 using System.Collections;
 
 public class CameraControl : MonoBehaviour 
@@ -9,6 +10,7 @@ public class CameraControl : MonoBehaviour
 	
 	// All private variables
 	private Vector3 m_OffsetPos2Player;
+	private Vector2 m_rangeVec;
 	// Use this for initialization
 	void Start () 
 	{
@@ -16,14 +18,21 @@ public class CameraControl : MonoBehaviour
 		{
 			Debug.LogError("No player found or player is not tagged!");
 		}
-		
-		m_OffsetPos2Player = transform.position - m_Player.position;
+		m_OffsetPos2Player = new Vector3(0.0f,10.0f,0.0f);
+		m_rangeVec = new Vector2(1.0f, 1.0f);
+		//m_OffsetPos2Player = transform.position - m_Player.position;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		Vector3 targetPos = m_Player.position + m_OffsetPos2Player;
-		transform.position = transform.position + (targetPos - transform.position) * m_PositionInterpolate;
+		float dx =  m_Player.position.x - transform.position.x, dz = m_Player.position.z - transform.position.z;
+		Vector3 deltaPosVec = new Vector3(0.0f, 0.0f, 0.0f);
+
+		if( Math.Abs(dx) > m_rangeVec.x  || Math.Abs(dz) > m_rangeVec.y){
+			deltaPosVec.x += (dx > 0)? dx-m_rangeVec.x : dx+m_rangeVec.x;
+			deltaPosVec.z += (dz > 0)? dz-m_rangeVec.y : dz+m_rangeVec.y;
+			transform.position = transform.position + deltaPosVec ;//+ (targetPos - transform.position) * m_PositionInterpolate;
+		}
 	}
 }
